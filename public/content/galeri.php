f64ec1057007ae290dfae1cf421af58f8303002f
Gabriellamaspaitela <125555145+Gabriellamaspaitela@users.noreply.github.com>
2023-02-16 19:40:12 +0700

    Add files via upload

---
+++ public/content/galeri.php
@@ -0,0 +1,28 @@
+<header class="container bg-warning  text-white">
+        <div class="row">
+            <div class="col-12 py-2 text-center">
+                <h2 class="display-6 ">Galleri </h2>
+            </div>
+        </div>
+        
+    </header>
+<main class="container border p-md-5 p-2">
+        <div class="row g-2 g-md-4">
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="geby1.jpg" alt="">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="geby2.jpg" alt="">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="geby3.png" alt="">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="geby4.png" alt="">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="geby5.png" alt="">
+            </div>
+            
+        </div>
+    </main
